06e21849faeb7e4098d14e2f883a569147d327e0
noob-ubaid <ubaidurrahman661@gmail.com>
2025-05-21 05:20:47 +0200

    Initial setup done

---
+++ index.js
@@ -0,0 +1,39 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const cors = require("cors");
+const port = process.env.PORT || 3000
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ubaid-database.njfi7n5.mongodb.net/?retryWrites=true&w=majority&appName=Ubaid-Database`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const dataBase = client.db("usersdb");
+    const usersCollection = dataBase.collection("users");
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+// assignment-10     7cu#Rek_a-R6fy_
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
