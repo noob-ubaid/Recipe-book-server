22db9ee264cab195c0756b921d39adc57ee5d264
noob-ubaid <ubaidurrahman661@gmail.com>
2025-05-21 20:09:16 +0200

    forgot to add await on get method

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const express = require('express')
-const app = express()
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const app = express();
+require("dotenv").config();
 const cors = require("cors");
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ubaid-database.njfi7n5.mongodb.net/?retryWrites=true&w=majority&appName=Ubaid-Database`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -11,7 +11,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -19,43 +19,40 @@ async function run() {
     await client.connect();
     const dataBase = client.db("usersdb");
     const usersCollection = dataBase.collection("recipe-book");
-    // get method 
-     app.get('/addrecipes', async (req,res) => {
-        const cursor = usersCollection.find();
-        const result =await cursor.toArray()
-        res.send(result)
-    })
-    app.get("/addrecipes/:id", async (req,res) => {
+    // get method
+    app.get("/addrecipes", async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/addrecipes/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)}
-      const result = usersCollection.findOne(query)
-      res.send(result)
-    })
-    // post method 
-    app.post("/addrecipes", async(req, res) => {
-        const newRecipe = req.body
-        const result = await usersCollection.insertOne(newRecipe);
-        res.send(result)
-    })
-
-
-
+      const query = { _id : new ObjectId(id) };
+      const result =await usersCollection.findOne(query);
+      res.send(result);
+    });
+    // post method
+    app.post("/addrecipes", async (req, res) => {
+      const newRecipe = req.body;
+      const result = await usersCollection.insertOne(newRecipe);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    
   }
 }
 run().catch(console.dir);
 
-
 app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 // assignment-10     7cu#Rek_a-R6fy_
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
