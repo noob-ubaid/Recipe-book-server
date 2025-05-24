785530ffa1808dccd6f63fd60d5925dcfd6b98cc
noob-ubaid <ubaidurrahman661@gmail.com>
2025-05-24 17:13:14 +0200

    added update recipe

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-    app.get('/filterrecipe', async (req,res) => {
-      const cursor = usersCollection.find().sort({like : -1}).limit(6)
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+    app.get("/filterrecipe", async (req, res) => {
+      const cursor = usersCollection.find().sort({ like: -1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     app.get("/addrecipes/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -61,14 +61,30 @@ async function run() {
       );
       res.send(result);
     });
+    app.put("/addrecipes/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedRecipe = req.body;
+      const updatedDoc = {
+        $set: updatedRecipe,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
 
-    // delete method 
-    app.delete("/addrecipes/:id", async (req , res) => {
-      const id = req.params.id
-      const query = {_id : new ObjectId(id)}
-      const result = await usersCollection.deleteOne(query)
-      res.send(result)
-    })
+      res.send(result);
+    });
+
+    // delete method
+    app.delete("/addrecipes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
