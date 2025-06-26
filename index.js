ebf697dc8cee20e866674353679d0bfd1589bb0d
noob-ubaid <ubaidurrahman661@gmail.com>
2025-06-26 14:20:38 +0200

    removed await.client()

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
+app.use(cors());
+app.use(express.json());
 const port = process.env.PORT || 3000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ubaid-database.njfi7n5.mongodb.net/?retryWrites=true&w=majority&appName=Ubaid-Database`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -16,7 +18,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
     const dataBase = client.db("usersdb");
     const usersCollection = dataBase.collection("recipe-book");
     // get method
@@ -26,7 +27,7 @@ async function run() {
       res.send(result);
     });
     app.get("/filterrecipe", async (req, res) => {
-      const cursor = usersCollection.find().sort({ like: -1 }).limit(6);
+      const cursor = usersCollection.find().sort({ like: -1 }).limit(8);
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -74,7 +75,6 @@ async function run() {
         updatedDoc,
         options
       );
-
       res.send(result);
     });
 
@@ -86,17 +86,13 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+  
   } finally {
   }
 }
 run().catch(console.dir);
 
-app.use(cors());
-app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
